831a9030b0016dc936c8649ab3a3eed46edc7181
Dio Deiva <ddwijaya@maybank.co.id>
2022-10-22 14:42:00 +0700

    fixing

--- src/features/Booking/Topic/useTopic.ts
+++ src/features/Booking/Topic/useTopic.ts
@@ -1,5 +1,5 @@
 const useTopic = () => {
-    return ['Proyeksi biaya kuliah & persiapannya sesuai Universitas pilihan Anda', 'Check kesehatan fianansial Anda saat ini', 'Check kesehatan fianansial Anda saat ini'];
+    return ['Proyeksi biaya kuliah & persiapannya sesuai Universitas pilihan Anda', 'Check kesehatan fianansial Anda saat ini', 'Investasi yang sesuai untuk mencapai target Anda\n'];
 };
 
 export default useTopic;
